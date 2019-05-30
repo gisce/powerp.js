6fc37be5e41ac27ccca7a330ea24812c3063d99b
Eduard Carreras <ecarreras@gisce.net>
2019-05-31 00:08:06 +0200

    Working

--- spec/PowerpSpec.js
+++ spec/PowerpSpec.js
@@ -1,67 +1,59 @@
 import Client from "../src/powerp";
-import chai from 'chai';
-import moxios from 'moxios';
 
-var expect = chai.expect;
-
-describe("a PowERP Client", function() {
-  describe('When initializing', function() {
-    it('should receive host, database, user, password', function() {
-      let c = new Client('http://localhost/api/v1', 'db', 'root', 'root');
-      expect(c).to.have.property('host');
-      expect(c).to.have.property('database');
-      expect(c).to.have.property('user');
-      expect(c).to.have.property('password');
+describe("a PowERP Client", () => {
+  describe('When initializing', () => {
+    it('should receive host, database, user, password', () => {
+      const c = new Client('http://localhost/api/v1', 'db', 'root', 'root');
+      expect(c.hasOwnProperty('host')).toBeTruthy();
+      expect(c.hasOwnProperty('database')).toBeTruthy();
+      expect(c.hasOwnProperty('user')).toBeTruthy();
+      expect(c.hasOwnProperty('password')).toBeTruthy();
     });
   });
-  describe('Getting a model', function() {
-    it('should return a Model instance', function() {
-      let c = new Client('http://localhost/api/v1', 'db', 'root', 'root');
-      let model = c.model('res.users');
-      expect(model.model).to.equal('res.users');
-      expect(model.client).to.equal(c);
+  describe('Getting a model', () => {
+    it('should return a Model instance', () => {
+      const c = new Client('http://localhost/api/v1', 'db', 'root', 'root');
+      const model = c.model('res.users');
+      expect(model.model).toEqual('res.users');
+      expect(model.client).toEqual(c);
     });
   });
 });
 
-describe("a Model", function() {
-  beforeEach(function () {
-    console.log('install mock');
-    moxios.install();
-  });
-  afterEach(function () {
-    moxios.uninstall();
-  });
-  describe("when reading", function() {
-    it("must call axios with the filter params, offset, limit", function() {
-      let c = new Client('http://localhost/api/v1', 'db', 'root', 'root');
-      let model = c.model('res.users');
-      model.read([['name', '=', 'admin']], ['name']).then(function(response) {
-        console.log("Response: " + response);
+describe("a Model", () => {
+
+  describe("when reading", () => {
+    it("must call post with the ids and fields", () => {
+      const c = new Client('http://localhost:8068', 'test_1559245558', 'admin', 'admin');
+      const model = c.model('res.partner');
+      model.read([1], ['name']).then(result => {
+        expect(result.length).toBe(1);
+        expect(result[0].id).toBe(1);
+        expect(result[0].hasOwnProperty('name')).toBeTruthy();
+      }).catch(reason => {
+        throw reason;
       });
-      console.log('palante no?');
-      moxios.wait(function() {
-        console.log('Fooo');
-        let request = moxios.requests.mostRecent();
-        console.log(request);
-        request.respondWith({
-          status: 200,
-          response: {
-              items: [
-                {
-                  id: 1,
-                  name: "Admin"
-                }
-              ],
-              n_items: 1,
-              limit: 80,
-              offset: 0
-          }
-        }).then(function() {
-          console.log('AAAAAAAAAa');
-          done();
-        })
-      });
-    })
+    });
   });
+
+  describe("when searching", () => {
+    it("must accept a search args", () => {
+      const c = new Client('http://localhost:8068', 'test_1559245558', 'admin', 'admin');
+      const model = c.model('res.partner');
+      model.search([['name', 'ilike', 'tiny sprl']]).then(result => {
+        expect(result.length).toBe(1);
+      });
+    });
+  });
+
+  describe("when creating", () => {
+    it("must create a new record", () => {
+      const c = new Client('http://localhost:8068', 'test_1559245558', 'admin', 'admin');
+      const model = c.model('res.partner');
+      model.create({name: 'GISCE'}).then(result => {
+        expect(typeof(result)).toBeGreaterThan(0);
+      });
+    });
+  });
+
 });
--- src/powerp/index.js
+++ src/powerp/index.js
@@ -1,4 +1,5 @@
-import axios from 'axios';
+import {encode, decode} from '@msgpack/msgpack';
+import 'whatwg-fetch';
 
 
 export default class Client {
@@ -21,37 +22,78 @@ class Model {
         this.model = model;
     }
 
-    search(params, offset, limit, context) {
+    async _fetch(payload) {
+        console.log(`Sending ${payload} to ${this.client.host}/object`);
+        try {
+            const response = await fetch(`${this.client.host}/object`, {
+                method: 'POST',
+                body: encode(payload),
+                headers: {
+                    'Content-Type': 'application/msgpack'
+                }
+            });
+            const buffer = await response.arrayBuffer();
+            const result =  decode(new Uint8Array(buffer));
+            console.debug(`Response from API: ${result}`);
+            return result;
+        } catch (e) {
+            console.log(`Error in decoding/fetching: ${e}`);
+            throw e;
+        }
+    }
+
+    async search(params, offset=0, limit=false, context=null) {
+        console.debug('searching');
+        const payload = [
+            'execute', this.client.database, 1,
+            this.client.password, this.model, 'search', params, offset, limit
+        ];
+        return this._fetch(payload);
+    }
+
+    async create(values) {
+        console.debug('creating');
+        const payload = [
+            'execute', this.client.database, 1,
+            this.client.password, this.model, 'create', values
+        ];
+        return this._fetch(payload);
 
     }
 
-    create(values) {
+    async write(ids, values) {
+        console.debug('write');
+        const payload = [
+            'execute', this.client.database, 1,
+            this.client.password, this.model, 'write', ids, values
+        ];
+        return this._fetch(payload);
+    }
+
+    async unlink(ids) {
+        console.debug('unlink');
+        const payload = [
+            'execute', this.client.database, 1,
+            this.client.password, this.model, 'unlink', ids
+        ];
+        return this._fetch(payload);
+    }
+
+    async read(ids, fields) {
+        console.log('reading');
+        //TODO: Use user id
+
+        const payload = [
+            'execute', this.client.database, 1,
+            this.client.password, this.model, 'read', ids, fields
+        ];
+        return this._fetch(payload);
 
     }
 
-    write(ids, values) {
-
-    }
-
-    unlink(ids) {
-
-    }
-
-    read(params, fields, offset=0, limit=80) {
-      console.log('reading');
-      return axios.get(this.client.host + '/' + this.model, {
-        params: {
-          filter: JSON.stringify(params),
-          fields: JSON.stringify(fields),
-          offset: offset,
-          limit: limit,
-        },
-      });
-    }
-
-    browse(ids) {
+    async browse(ids) {
         let records = [];
-        ids.forEach(function(id, index, array) {
+        ids.forEach((id) => {
             records.push(new Record(this, id));
         });
         return records;
@@ -63,18 +105,18 @@ class Record {
     constructor(model, id) {
         this.model = model;
         this.id = id;
-        this.data = this.read([])
+        this.data = this.read(this.id, [])
     }
 
-    write(values) {
-        this.model.write([id], values);
+    async write(values) {
+        this.model.write([this.id], values);
     }
 
-    unlink() {
-        this.model.unlink([id]);
+    async unlink() {
+        this.model.unlink([this.id]);
     }
 
-    read(fields) {
-        return this.model.read([this.id], fields)[0]
+    async read(fields) {
+        return this.model.read(this.id, fields)
     }
 }
