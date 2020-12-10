76c03938e920fe4abeba01992e95d794edeab5d7
Eduard Carreras <ecarreras@gisce.net>
2020-12-10 09:13:59 +0100

    Modify test

--- spec/PowerpSpec.js
+++ spec/PowerpSpec.js
@@ -1,19 +1,24 @@
 import Client from "../src/powerp";
 
+const BACKEND_URL = '';
+const BACKEND_DATABASE = '';
+
 describe("a PowERP Client", () => {
   describe('When initializing', () => {
-    it('should receive host, database, user, password', () => {
-      const c = new Client('http://localhost/api/v1', 'db', 'root', 'root');
+    it('should receive host, database', () => {
+      const c = new Client('https://localhost:8068', 'db');
       expect(c.hasOwnProperty('host')).toBeTruthy();
+      expect(c.host).toBe('https://localhost:8068');
       expect(c.hasOwnProperty('database')).toBeTruthy();
-      expect(c.hasOwnProperty('user')).toBeTruthy();
-      expect(c.hasOwnProperty('password')).toBeTruthy();
+      expect(c.database).toBe('db');
+      expect(c.hasOwnProperty('token')).toBeTruthy();
+      expect(c.token).toBeNull();
     });
 
     it('should allow login', (done) => {
-      const c = new Client('http://localhost:8068', 'test_1559245558', 'admin', 'admin');
-      c.login().then(() => {
-        expect(c.uid).toBe(1);
+      const c = new Client(BACKEND_URL, BACKEND_DATABASE);
+      c.login('test', 'test').then(() => {
+        expect(c.token).toBeTruthy();
         done();
       }).catch(done);
     })
@@ -21,7 +26,7 @@ describe("a PowERP Client", () => {
   });
   describe('Getting a model', () => {
     it('should return a Model instance', () => {
-      const c = new Client('http://localhost/api/v1', 'db', 'root', 'root');
+      const c = new Client(BACKEND_URL, BACKEND_DATABASE);
       const model = c.model('res.users');
       expect(model.model).toEqual('res.users');
       expect(model.client).toEqual(c);
@@ -34,9 +39,9 @@ describe("a Model", () => {
 
     describe("when reading", () => {
       it("must call post with the ids and fields", (done) => {
-        const c = new Client('http://localhost:8068', 'test_1559245558', 'admin', 'admin');
+        const c = new Client(BACKEND_URL, BACKEND_DATABASE);
         const model = c.model('res.partner');
-        c.login().then(() => {
+        c.login('test', 'test').then(() => {
           model.read([1], ['name']).then(result => {
             expect(result.length).toBe(1);
             expect(result[0].id).toBe(1);
@@ -49,10 +54,10 @@ describe("a Model", () => {
 
     describe("when searching", () => {
       it("must accept a search args", (done) => {
-        const c = new Client('http://localhost:8068', 'test_1559245558', 'admin', 'admin');
+        const c = new Client(BACKEND_URL, BACKEND_DATABASE);
         const model = c.model('res.partner');
-        c.login().then(() => {
-          model.search([['name', 'ilike', 'tiny sprl']]).then(result => {
+        c.login('test', 'test').then(() => {
+          model.search([['id', '=', '1']]).then(result => {
             expect(result.length).toBe(1);
             done();
           }).catch(done);
@@ -62,9 +67,9 @@ describe("a Model", () => {
 
     describe("when creating", () => {
       it("must create a new record", (done) => {
-        const c = new Client('http://localhost:8068', 'test_1559245558', 'admin', 'admin');
+        const c = new Client(BACKEND_URL, BACKEND_DATABASE);
         const model = c.model('res.partner');
-        c.login().then(() => {
+        c.login('test', 'test').then(() => {
           model.create({name: 'GISCE'}).then(result => {
             expect(result).toBeGreaterThan(0);
             done();
@@ -75,9 +80,9 @@ describe("a Model", () => {
 
     describe('when browsing', () => {
       it('should return a list of records', (done) => {
-        const c = new Client('http://localhost:8068', 'test_1559245558', 'admin', 'admin');
+        const c = new Client(BACKEND_URL, BACKEND_DATABASE);
         const model = c.model('res.partner');
-        c.login().then(() => {
+        c.login('test', 'test').then(() => {
           model.search([]).then((ids) => {
             const records = model.browse(ids).then((records) => {
               expect(records.length).toBe(ids.length);
@@ -90,9 +95,9 @@ describe("a Model", () => {
 
     describe('when calling undefined method in library', () =>{
       it('should call the method in the backend', (done) => {
-        const c = new Client('http://localhost:8068', 'test_1559245558', 'admin', 'admin');
+        const c = new Client(BACKEND_URL, BACKEND_DATABASE);
         const model = c.model('res.partner');
-        c.login().then(() => {
+        c.login('test', 'test').then(() => {
           model.fields_get(['name']).then((result) => {
             expect(result.hasOwnProperty('name')).toBeTruthy();
             expect(result.name.hasOwnProperty('required')).toBeTruthy();
