b07f3df41a1de931fc8eb7117ec1ef1b6d01ce64
Marc Güell Segarra <marc@ondori.dev>
2020-12-20 10:27:20 +0100

    Fix dist folder issues & export

--- dist/model.d.ts
+++
@@ -1,4 +0,0 @@
-import { ModelConnectionOpts, ModelSearchOpts } from "./types";
-export declare abstract class Model {
-    static search(options: ModelSearchOpts, connection: ModelConnectionOpts): Promise<void>;
-}
