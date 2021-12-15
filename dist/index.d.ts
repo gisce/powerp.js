4dd82e5a25f947d761aeaccaf2c6f5d0b18fd8d4
Marc Güell Segarra <marc@ondori.dev>
2021-12-15 12:29:16 +0100

    Remove dist folder from git

--- dist/index.d.ts
+++
@@ -1,3 +0,0 @@
-export { Client } from "./client";
-export { Model } from "./model";
-export { Report } from "./report";
--- dist/report.d.ts
+++
@@ -1,8 +0,0 @@
-import { Client } from "./client";
-import { CreateReportOpts, GetReportOpts } from "./types";
-export declare class Report {
-  client: Client;
-  constructor(client: Client);
-  create(options: CreateReportOpts): Promise<any>;
-  get(options: GetReportOpts): Promise<any>;
-}
