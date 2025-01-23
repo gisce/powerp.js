5b539c0212f40eb48153853c1f52ecc6eb0b0c56
Marc Güell Segarra <marc@ondori.dev>
2025-01-23 11:22:00 +0100

    Revert "Merge branch 'fix/no-referrer-policy' into alpha"
    This reverts commit c34853ad9541e59ddc900b1b56a93adb3d877846, reversing
    changes made to f0948919fb7d8fa3cea681fe9c4e6575c31cf61c.

--- lib/client.ts
+++ lib/client.ts
@@ -54,14 +54,6 @@ export class Client {
           "Referrer-Policy": "no-referrer",
         },
       });
-      this.axiosInstance.interceptors.request.use((config) => {
-        if (config.headers) {
-          config.headers.Referer = "";
-          config.headers.referrer = "";
-          config.headers.Referrer = "";
-        }
-        return config;
-      });
     }
     return this.axiosInstance;
   }
